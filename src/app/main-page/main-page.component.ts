1f82b8d3ac5df6d8dfb9bc119e8c55ba920f223a
andriy-skoroppad <andry.skoropad@gmail.com>
2019-07-13 21:25:38 +0300

    migration

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit,  OnDestroy} from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 import { Localstore } from '../service/localstore.service';
 import { EditPopup } from '../popups/edit/popup-edit.component';
--- src/app/popups/qrcode/popup-qrcode.component.ts
+++ src/app/popups/qrcode/popup-qrcode.component.ts
@@ -1,5 +1,5 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Sinc } from '../../service/sinc.service';
 import { Api } from '../../service/api.service';
 import { Localstore } from '../../service/localstore.service';
